206d9264a5dcbade5865145dcfa85ef8604a1e94
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-06-28 23:51:20 +0800

    remove current sound if > 100 distance

--- client.lua
+++ client.lua
@@ -10,16 +10,18 @@ end)
 
 Citizen.CreateThread(function()
     while true do
-        local mycoords = GetEntityCoords(PlayerPedId())
-        for k,v in pairs(GetGamePool('CVehicle')) do
-            if #(mycoords - GetEntityCoords(v, false)) < 100 then
-                local plate = string.gsub(GetVehicleNumberPlateText(v), "%s+", "")
-                if vehicle_sounds[plate] ~= nil and vehicle_sounds[plate].plate ~= nil and plate == vehicle_sounds[plate].plate and vehicle_sounds[plate].current ~= vehicle_sounds[plate].name then
-                    ForceVehicleEngineAudio(v,vehicle_sounds[plate].name)
-                    vehicle_sounds[plate].current = vehicle_sounds[plate].name
-                end
+      local mycoords = GetEntityCoords(PlayerPedId())
+      for k,v in pairs(GetGamePool('CVehicle')) do
+          if #(mycoords - GetEntityCoords(v, false)) < 100 then
+            local plate = string.gsub(GetVehicleNumberPlateText(v), "%s+", "")
+            if vehicle_sounds[plate] ~= nil and vehicle_sounds[plate].plate ~= nil and plate == vehicle_sounds[plate].plate and vehicle_sounds[plate].current ~= vehicle_sounds[plate].name then
+                ForceVehicleEngineAudio(v,vehicle_sounds[plate].name)
+                vehicle_sounds[plate].current = vehicle_sounds[plate].name
             end
-        end
+          elseif #(mycoords - GetEntityCoords(v, false)) > 100 and vehicle_sounds[plate] ~= nil and vehicle_sounds[plate].current ~= nil then
+            vehicle_sounds[plate].current = nil
+          end
+      end
         Wait(2000)
     end
 end)
